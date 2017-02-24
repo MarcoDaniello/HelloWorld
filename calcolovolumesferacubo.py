b7befaa4c54a4fed1da03940cf3fd98da45b4353
MarcoDaniello <marcod448@gmail.com>
2017-02-24 19:40:22 +0100

    Add files via upload

---
+++ calcolovolumesferacubo.py
@@ -0,0 +1,14 @@
+import math
+print "programma che calcola volume di un cubo o di una sfera in base alla scelta dell'utente" 
+print "Effettua scelta 1=cubo, 2=sfera"
+s = input ("?")
+
+if (s==1):
+   lato = input("Inserisci lato del cubo ")
+   volume = lato*lato*lato
+   print ("Il volume del cubo risulta essere"), volume
+elif s==2:
+   raggio = input("Inserisci raggio ")
+   volume = 4. / 3. * math.pi * raggio * raggio * raggio
+   print ("il volume della sfera risulta essere"), volume
+   
