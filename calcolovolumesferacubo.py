74101d92fadce97c92efd049464626092c9e40fc
MarcoDAniello <marcod448@gmail.com>
2017-03-17 15:33:59 +0100

    input

--- calcolovolumesferacubo.py
+++ calcolovolumesferacubo.py
@@ -1,7 +1,6 @@
 import math
 print "programma che calcola volume di un cubo o di una sfera in base alla scelta dell'utente" 
-print "Effettua scelta 1=cubo, 2=sfera"
-s = input ("?")
+s = input ( "Effettua scelta 1=cubo, 2=sfera")
 
 if (s==1):
    lato = input("Inserisci lato del cubo ")
