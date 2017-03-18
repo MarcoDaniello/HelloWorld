da1e66de0fd909523a4d9b78658bf2116c8241a4
MarcoDAniello <marcod448@gmail.com>
2017-03-18 17:34:25 +0100

    modifica else

--- paridisp.py
+++ paridisp.py
@@ -1,5 +1,6 @@
+print "Programma che verifica se un numero e'  pari o dispari"
 n = input("Inserisci valore")
 if n % 2 == 0:
-     print "pari"
-elif n % 2 == 1:
-     print "dispari"
+     print "Il numero e'  pari"
+else n % 2 == 1:
+     print "Il numero e'  dispari"
