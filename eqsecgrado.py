49bd85ede2069b0c715ac6e90f740bb77b0ff251
Antonio <ciao@gmail.com>
2017-04-07 10:36:23 +0200

    modifica

--- eqsecgrado.py
+++ eqsecgrado.py
@@ -3,34 +3,34 @@ import math
 print "Risolve equazioni di secondo grado"
 
 
-a = input('a: ')
-b = input('b: ')
-c = input('c: ')
+A = input('A: ')
+B = input('B: ')
+C = input('C: ')
 
-if a==0 and b==0 and c==0:
+if A==0 and B==0 and C==0:
     print "L'equazione 0 = 0 ammette infinite soluzioni"
 
-elif a==0 and b==0:
-    print "L'equazione", c, "= 0 non ammette soluzioni"
+elif A==0 and B==0:
+    print "L'equazione non ammette soluzioni"
 
-elif a==0:
+elif A==0:
     print "L'equazione si riduce al primo grado"
-    print "Soluzione:", -float(c) / float(b)
+    print  -float(C) / float(B)
 
 else:
-    delta = b * b - 4 * a * c
+    delta = B * B - 4 * A * C
     if delta < 0:
-        print "Delta < 0. L'equazione ammette due soluzioni"
+        print " L'equazione ammette due soluzioni"
         rad_delta = math.sqrt(-delta)
-        re = -float(b) / (2.0 * float(a))
-        imm = abs(rad_delta / (2.0 * float(a)))
+        re = -float(B) / (2.0 * float(A))
+        imm = abs(rad_delta / (2.0 * float (A)))
         print "x1 =", re, "-i", imm
         print "x2 =", re, "+i", imm
     elif delta == 0:
-        print "Delta = 0. L'equazione ammette due soluzioni coincidenti"
-        print "x1 = x2 =", -float(b) / (2.0 * float(a))
+        print " L'equazione ammette due soluzioni coincidenti"
+        print "x1 = x2 =", -float(B) / (2.0 * float(A))
     else:
         print "L'equazione ammette due soluzioni distinte"
         rad_delta = math.sqrt(delta)
-        print "x1 =", (-float(b) - rad_delta) / (2.0 * float(a))
-print "x2 =", (-float(b) + rad_delta) / (2.0 * float(a))
+        print "x1 =", (-float(B) - rad_delta) / (2.0 * float(A))
+print "x2 =", (-float(A) + rad_delta) / (2.0 * float(A))
